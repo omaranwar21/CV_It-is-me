afd253a9b84bc29e5dcbaba31f03e619aca38018
omaranwar21 <omarahmed11185@gmail.com>
2023-05-10 21:46:45 +0300

    Update PCA2.cpp

--- PCA2.cpp
+++ PCA2.cpp
@@ -14,6 +14,8 @@ vector<Mat> readImages(vector<string> trainFacesPath)
         string file_path = folder_path + "\\" + filename;
         Mat img = imread(file_path);
         resize(img, img, Size(100, 100));
+        //convert to gray
+        cvtColor(img, img, COLOR_BGR2GRAY);
         images.push_back(img);
     }
 
@@ -48,42 +50,77 @@ vector<Mat> readList(string listFilePath)
     return images;
 }
 
+float Average_Face(Mat image){
+    // Mat mean; 
+    float mean=0 ;
+    // loop on image
+    for (int c = 0; c < image.cols; c++){
+        for (int r=0; r<image.rows; r++){
+            mean += image.at<uchar>(r, c);
+        }
+    }
+    mean = mean / (image.rows * image.cols);
+
+    return mean;
+}
+
 Mat PCA_Matrix(vector<Mat> images){
     Mat data ;
     // average vector
-    vector<float> means;
+    // vector<float> means;
     
     
     int n = (int)images.size();
     int d = images[0].rows * images[0].cols;
+    // cout<< n <<"," << d << endl;
     data = Mat(d, n, images[0].type());
+    // cout<< data.rows << endl;
+    // cout<< data.cols << endl;
     // int counter = 0;
 
     for (int i = 0; i < n; i++){
 
         // means.pushback(Average_Face)
-        means.push_back(Average_Face(images[i]));
+        float mean = Average_Face(images[i]);
+        // imshow("image wo avg", images[i]);
+        images[i] = images[i]- (int)mean;
+
         Mat image = images[i].clone().reshape(0, d);
         image.convertTo(image, images[0].type());
         image.copyTo(data.col(i));
     }
-    return data;
-}
+    // transpose data image
+    // cout << data.t().size() << endl;
+    // imshow("ttttt", data.t());
+    // cout<< ((data)*(data.t())).size() << endl;
 
-float Average_Face(Mat image){
-    // Mat mean; 
-    float mean=0 ;
-    // loop on image
-    for (int c = 0; c < image.cols;c++){
-        for (int r=0; r<image.rows,r++){
-            mean += image.at<float>(r, c);
+    // Mat covarianceMatrix = (Mat_<uchar>(10000, 10000));
+    Mat covarianceMatrix(data.rows, data.rows, data.type());
+    // covarianceMatrix = (data).cross((data.t()));
+
+    Mat data_transpose = data.t();
+    // cout << data_transpose.size() << endl;
+    // cout << data.size() << endl;
+
+
+    // Multiply matrix A and transposed matrix B
+    for (int i = 0; i < data.rows; i++) {
+        for (int j = 0; j < data.rows; j++) {
+            int sum = 0;
+            for (int k = 0; k < data.cols; k++) {
+                sum += data.at<uchar>(i,k) * data_transpose.at<uchar>(k,j);
+            }
+            covarianceMatrix.at<uchar>(i,j) = sum;
         }
     }
-    mean = mean / (image.rows * image.cols);
-
-    return mean
-
+    // cout<< covarianceMatrix.rows << endl;
+    // data = data.t();
+    return covarianceMatrix;
 }
+
+
+
+
 
 
 int main(int argc, char const *argv[])
@@ -94,8 +131,8 @@ int main(int argc, char const *argv[])
 
     // get the mean of each column
 
-    cout<< data.rows << endl;
-    cout<< data.cols << endl;
+    // cout<< data.rows << endl;
+    // cout<< data.cols << endl;
     
     //resize image
     imshow("Image", data);
