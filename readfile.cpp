7bccb5d4062f2affdd715d2f63cca1ff53b6e819
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2023-05-10 17:01:01 +0300

    read files

---
+++ readfile.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+#include <sstream>
+#include <string>
+
+#include <fstream>
+// #include <unistd.h>
+
+
+using namespace std;
+using namespace cv;
+//read training list
+void readList(string& listFilePath, vector<string>& facesPath, vector<string>& facesID)
+{
+    ifstream file(listFilePath.c_str(), ifstream::in);
+    
+    if (!file) {
+        cout << "Fail to open file: " << listFilePath << endl;
+        exit(0);
+    }
+    
+    string line, path, id;
+    while (getline(file, line)) {
+        stringstream lines(line);
+        getline(lines, id, ';');
+        getline(lines, path);
+        
+        path.erase(remove(path.begin(), path.end(), '\r'), path.end());
+        path.erase(remove(path.begin(), path.end(), '\n'), path.end());
+        path.erase(remove(path.begin(), path.end(), ' '), path.end());
+        
+        facesPath.push_back(path);
+        //facesID.push_back(atoi(id.c_str()));
+        facesID.push_back(id);
+    }
+}
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string trainListFilePath = "E:\\SBME 6th Term\\Computer Vision\\Projects & Tasks\\CV Final Project\\Repo\\CV_It-is-me\\list\\train_list.txt";
+    vector<string> trainFacesPath;
+    vector<string> trainFacesID;
+    // vector<string> loadedFacesID;
+    //read training list and ID from txt file
+    readList(trainListFilePath, trainFacesPath, trainFacesID);
+
+    // print trainFacesPath 
+    for (int i = 0; i < trainFacesPath.size(); i++)
+    {
+        cout << trainFacesPath[i] << endl;
+    }
+
+    return 0;
+
+}
